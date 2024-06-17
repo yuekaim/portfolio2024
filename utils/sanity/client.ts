b0a6e4d85904158c09b4823bef566352435cd708
yuekaim <yuekaim@gmail.com>
2024-06-18 00:47:39 +0200

    hide project id

--- utils/sanity/client.ts
+++ utils/sanity/client.ts
@@ -2,7 +2,7 @@
 import { createClient } from "next-sanity"
 
 export const client = createClient({
-    projectId: '4ksy39ot',
+    projectId: process.env.SANITY_PROJECT_ID,
     dataset: 'production',
     useCdn: true, // set to `false` to bypass the edge cache
     apiVersion: '2024-06-03', // use current date (YYYY-MM-DD) to target the latest API version

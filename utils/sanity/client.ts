bfbbd7cf6966c7103327e136bfacd46d77e717c3
yuekaim <yuekaim@gmail.com>
2024-07-05 02:55:31 +0200

    dont use cdn

--- utils/sanity/client.ts
+++ utils/sanity/client.ts
@@ -4,6 +4,6 @@ import { createClient } from "next-sanity"
 export const client = createClient({
     projectId: process.env.SANITY_PROJECT_ID,
     dataset: 'production',
-    useCdn: true, // set to `false` to bypass the edge cache
+    useCdn: false, // set to `false` to bypass the edge cache
     apiVersion: '2024-06-03', // use current date (YYYY-MM-DD) to target the latest API version
 })

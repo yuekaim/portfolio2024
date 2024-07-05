b1f4b225e69fc3082d3c5d8e11092cb7b6cabc66
yuekaim <yuekaim@gmail.com>
2024-07-05 18:55:54 +0200

    categories center

--- app/components/navigation/categories/index.tsx
+++ app/components/navigation/categories/index.tsx
@@ -65,7 +65,7 @@ const Categories: React.FC = ({}) => {
           transformOrigin: 'center'
         }}
           >
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" className='mt-6 h-10 top-10'
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" className=''
         style={{
           transform: `scale(2)`, 
         }}>
